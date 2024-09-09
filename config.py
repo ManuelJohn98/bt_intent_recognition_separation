38380029ea71902b7c7dd1c1ffd008a12fbaa396
ManuelJohn98 <github.com.fidgeting219@passmail.net>
2024-09-10 01:18:03 +0200

    add global config

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+# set root directory for this project
+root_directory = os.path.dirname(os.path.abspath(__file__))
+# set data directory
+data_directory = os.path.join(root_directory, "data")
+# set raw data directory
+raw_data_directory = os.path.join(data_directory, "raw_data")
+# TODO: set model directory
