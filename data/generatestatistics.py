58f49ea293c738ae0a54a75bc2ce5af5f3628955
ManuelJohn98 <github.com.fidgeting219@passmail.net>
2024-09-10 02:44:31 +0200

    update to fit into program architecture

--- data/generatestatistics.py
+++ data/generatestatistics.py
@@ -1,37 +1,29 @@
 # generatestatistics.py
 
+import os
 import re
+from pprint import pprint
+from config import raw_data_directory, statistics_directory
 
-FILES = [
-    "Halleneroeffnung_A-DRZ_1", 
-    "Halleneroeffnung_A-DRZ_2", 
-    "Halleneroeffnung_A-DRZ_3",
-    "Halleneroeffnung_Durchgang_1", 
-    "Halleneroeffnung_Durchgang_2", 
-    "Halleneroeffnung_Durchgang_3",
-    "Halleneroeffnung_Durchgang_4",
-    "Viersen_test1_1", 
-    "Viersen_test2_1",
-    "Viersen_test3"
-    ]
 
-label_dict = {'multi_intents': 0}
-
-def statistics(filename):
+def _statistics_raw_data_from_file(filename) -> dict:
+    filename = os.path.join(raw_data_directory, filename)
+    label_dict = {"multi_intents": 0}
     new_paragraph = True
-    current_label = ''
+    current_label = ""
 
-    with open(filename, 'r', encoding='utf-8') as f:
+    with open(filename, "r", encoding="utf-8") as f:
         for line in f.readlines():
-            if line == '\n':
+            if line == "\n":
                 new_paragraph = True
                 continue
-            if line.startswith('#'):
+            if line.startswith("#"):
                 continue
-            elems = line.split(sep='\t')
-            label = re.match(r'[a-z_\\\/]+', elems[3]).group(0)
-            if label == '_':
-                continue
+            elems = line.split(sep="\t")
+            label = re.match(r"[a-z_\\\/]+", elems[3]).group(0)
+            label = label.replace("\\", "")
+            if label == "_":
+                label = "other"
             if new_paragraph:
                 if label in label_dict:
                     label_dict[label] += 1
@@ -45,16 +37,28 @@ def statistics(filename):
                 else:
                     label_dict[label] = 1
                 current_label = label
-                label_dict['multi_intents'] += 1
+                label_dict["multi_intents"] += 1
     return label_dict
 
-for file in FILES:
-    file = file + '.tsv'
-    statistics(file)
 
+def write_statistics_raw_data():
+    label_dict = dict()
+    for file in os.listdir(raw_data_directory):
+        # add statistics of the current file to the label_dict
+        other_label_dict = _statistics_raw_data_from_file(file)
+        for label in other_label_dict:
+            if label in label_dict:
+                label_dict[label] += other_label_dict[label]
+            else:
+                label_dict[label] = other_label_dict[label]
+    # sum all values except multi_intents
+    label_dict["total"] = sum(
+        [label_dict[label] for label in label_dict if label != "multi_intents"]
+    )
+    pprint(label_dict)
 
-with open("statistics.txt", 'w', encoding='utf-8') as f:
-    # f.write(f"# {filename}\n")
-    for label in label_dict:
-        f.write(f"{label} - {label_dict[label]}\n")
-    f.write('\n')
+    filename = os.path.join(statistics_directory, "raw_data_statistics.txt")
+    with open(filename, "w", encoding="utf8") as f:
+        # f.write(f"# {filename}\n")
+        for label in label_dict:
+            f.write(f"{label} - {label_dict[label]}\n")
