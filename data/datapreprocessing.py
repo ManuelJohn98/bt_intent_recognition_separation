b48ec5a2f7363ca5976d58cd31f16c480ec61c46
majo98 <manuel.john@dfki.de>
2024-09-17 10:53:41 +0200

    adapt formatting to huggingface format

--- data/datapreprocessing.py
+++ data/datapreprocessing.py
@@ -29,6 +29,7 @@ def _preprocess_raw_data(file: str, ext: str = "tsv") -> list:
     STAT = load_config()["statistics"]
     file = os.path.join(raw_data_directory, file)
     labeled_data = []
+    labels = set()
     with open(file, "r", encoding="utf8") as f:
         current_turn_no = 0
         current_label = ""
@@ -48,13 +49,16 @@ def _preprocess_raw_data(file: str, ext: str = "tsv") -> list:
             elements = line.split(sep)  # line is guaranteed to have 4 elements
             turn_id = elements[0]
             turn_no = int(turn_id.split("-")[0])
-            content = elements[2]
+            token = elements[2]
             # get the lable which is prefix of last element
             label_with_count = elements[3].split("[")[0].replace("\\", "")
             # convert labels to BIO format
             if label_with_count == "_":
                 current_label = ""
-                labeled_data_point[content] = "O"
+                labeled_data_point["id"] = turn_no
+                labeled_data_point["label"] = "O"
+                labeled_data_point["token"] = token
+                labels.add("O")
                 if STAT:
                     stats = StatisticsCollector()
                     stats.count_labeled("data", "other")
@@ -64,24 +68,51 @@ def _preprocess_raw_data(file: str, ext: str = "tsv") -> list:
                 current_label = label_with_count
                 # sometimes the label has a number at the end
                 label = _remove_count_from_label(label_with_count)
-                labeled_data_point[content] = "B-" + label
+                labeled_data_point["id"] = turn_no
+                labeled_data_point["label"] = "B-" + label
+                labeled_data_point["token"] = token
+                labels.add("B-" + label)
                 if STAT:
                     stats = StatisticsCollector()
                     stats.count_labeled("data", label)
             elif current_label != label_with_count:
                 current_label = label_with_count
                 label = _remove_count_from_label(label_with_count)
-                labeled_data_point[content] = "B-" + label
+                labeled_data_point["id"] = turn_no
+                labeled_data_point["label"] = "B-" + label
+                labeled_data_point["token"] = token
+                labels.add("B-" + label)
                 if STAT:
                     stats = StatisticsCollector()
                     stats.count_labeled("data", label)
                     stats.count_unlabeled("need_for_separation")
             else:
                 # only use I for these intermediary labels without actual label
-                labeled_data_point[content] = "I"
+                labeled_data_point["id"] = turn_no
+                labeled_data_point["label"] = "I"
+                labeled_data_point["token"] = token
+                labels.add("I")
             # add the labaled data point to labeled data
             labeled_data.append(labeled_data_point)
+    labeled_data = [sorted(labels)] + labeled_data
     return labeled_data
+
+
+def _transform_to_huggingface_format(labeled_data: list) -> list:
+    huggingface_format = list()
+    labels = labeled_data[0]
+    current_turn_no = 0
+    for data_point in labeled_data[1:]:
+        turn_no = data_point["id"]
+        if current_turn_no != turn_no:
+            current_turn_no = turn_no
+            huggingface_format.append(dict())
+            huggingface_format[-1]["id"] = turn_no
+            huggingface_format[-1]["labels"] = []
+            huggingface_format[-1]["tokens"] = []
+        huggingface_format[-1]["labels"].append(labels.index(data_point["label"]))
+        huggingface_format[-1]["tokens"].append(data_point["token"])
+    return huggingface_format
 
 
 # def _concat_json(json1: dict, json2: dict) -> dict:
@@ -97,10 +128,14 @@ def convert_all_raw_data(exentension: str) -> None:
     labeled_data = []
     if exentension != "":
         for file in _get_file_names_from_dir(exentension):
-            labeled_data = labeled_data + _preprocess_raw_data(file, exentension)
+            labeled_data = labeled_data + _transform_to_huggingface_format(
+                _preprocess_raw_data(file, exentension)
+            )
     else:
         for file in _get_file_names_from_dir():
-            labeled_data = labeled_data + _preprocess_raw_data(file)
+            labeled_data = labeled_data + _transform_to_huggingface_format(
+                _preprocess_raw_data(file)
+            )
     with open(
         os.path.join(data_directory, "processed_data.json"), "w", encoding="utf8"
     ) as f:
